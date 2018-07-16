cc361aae64e5cb32b93d0743664d3025578c0451
Bernardo António Borda d'Água <berna.agua@campus.fct.unl.pt>
2018-07-16 19:07:21 +0100

    Animações, Tiles, Gates, Room, NextLevel

--- Scenes/world/Godot.gd
+++ Scenes/world/Godot.gd
@@ -23,16 +23,25 @@ func _move():
 	
 	if Input.is_action_pressed("ui_d"):
 		_update_motion_x(right)
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.set_flip_h(false)
 		
 	if Input.is_action_pressed("ui_a"):
 		_update_motion_x(left)
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.set_flip_h(true)
 		
 	if Input.is_action_pressed("ui_s"):
 		_update_motion_y(down)
+		$AnimatedSprite.play("walk")
 		
 	if Input.is_action_pressed("ui_w"):
 		_update_motion_y(up)
-		
+		$AnimatedSprite.play("walk")
+	
+	if motion == Vector2(0,0):
+		$AnimatedSprite.play("idle")
+			
 	motion = motion.normalized() * speed
 	
 	motion = move_and_slide(motion)
@@ -41,5 +50,6 @@ func _new_room():
 	pass
 	
 func _physics_process(delta):
+	
 	_move()
 	
